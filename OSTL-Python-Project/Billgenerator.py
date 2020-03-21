ba9a12ed0bf73f4f0a8acef777d1bd0f6524bec9
WHITEDEVIL <61189997+WHITEDEVIL2601@users.noreply.github.com>
2020-03-21 11:10:52 -0400

    Add files via upload

---
+++ OSTL-Python-Project/Billgenerator.py
@@ -0,0 +1,164 @@
+from tkinter import *
+import random
+
+
+root = Tk()
+root.geometry("900x500+0+0")
+root.title(" HUMA CATERER BILL GENERATOR")
+
+Tops = Frame(root,bg="white",width = 1600,height=50,relief=SUNKEN)
+Tops.pack(side=TOP)
+
+f1 = Frame(root,width = 900,height=700,relief=SUNKEN)
+f1.pack(side=LEFT)
+
+lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="HUMA CATERER BILL GENERATOR",fg="gray20",bd=10,anchor='w')
+lblinfo.grid(row=0,column=0)
+
+lblinfo.grid(row=1,column=0)
+
+
+def Ref():
+    x=random.randint(12980, 50876)
+    randomRef = str(x)
+    rand.set(randomRef)
+    cof =float(Fries.get())
+    colfries= float(Largefries.get())
+    cob= float(Burger.get())
+    cofi= float(Filet.get())
+    cochee= float(Cheese_burger.get())
+    costoffries = cof*25
+    costoflargefries = colfries*40
+    costofburger = cob*35
+    costoffilet = cofi*50
+    costofcheeseburger = cochee*50
+   
+    subtotal=(costoffries +  costoflargefries + costofburger + costoffilet  + costofcheeseburger)
+    PayTax=((costoffries +  costoflargefries + costofburger + costoffilet +  costofcheeseburger )*0.05)
+    Totalcost=(costoffries +  costoflargefries + costofburger + costoffilet  + costofcheeseburger )
+
+    OverAllCost="Rs.",str( PayTax + Totalcost )
+    PaidTax="Rs.",str('%.2f'% PayTax)
+    Tax.set(PaidTax)
+    Subtotal.set(subtotal)
+    Total.set(OverAllCost)
+
+
+def qexit():
+    root.destroy()
+
+def reset():
+    rand.set("")
+    Fries.set("")
+    Largefries.set("")
+    Burger.set("")
+    Filet.set("")
+    Subtotal.set("")
+    Total.set("")
+    
+  
+    Tax.set("")
+    Cheese_burger.set("")
+
+rand = StringVar()
+Fries = StringVar()
+Largefries = StringVar()
+Burger = StringVar()
+Filet = StringVar()
+Subtotal = StringVar()
+Total = StringVar()
+Tax = StringVar()
+Cheese_burger = StringVar()
+
+lblfries = Label(f1, font=( 'aria' ,16, 'bold' ),text="Fries",fg="steel blue",bd=10,anchor='w')
+lblfries.grid(row=1,column=0)
+txtfries = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Fries , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtfries.grid(row=1,column=1)
+
+lblLargefries = Label(f1, font=( 'aria' ,16, 'bold' ),text="Lunch Meal",fg="steel blue",bd=10,anchor='w')
+lblLargefries.grid(row=2,column=0)
+txtLargefries = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Largefries , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtLargefries.grid(row=2,column=1)
+
+
+lblburger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Burger",fg="steel blue",bd=10,anchor='w')
+lblburger.grid(row=3,column=0)
+txtburger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtburger.grid(row=3,column=1)
+
+lblFilet = Label(f1, font=( 'aria' ,16, 'bold' ),text="Pizza",fg="steel blue",bd=10,anchor='w')
+lblFilet.grid(row=4,column=0)
+txtFilet = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Filet , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtFilet.grid(row=4,column=1)
+
+lblCheese_burger = Label(f1, font=( 'aria' ,16, 'bold' ),text="Cheese burger",fg="steel blue",bd=10,anchor='w')
+lblCheese_burger.grid(row=5,column=0)
+txtCheese_burger = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Cheese_burger , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtCheese_burger.grid(row=5,column=1)
+
+
+lblSubtotal = Label(f1, font=( 'aria' ,16, 'bold' ),text="Subtotal",fg="steel blue",bd=10,anchor='w')
+lblSubtotal.grid(row=3,column=2)
+txtSubtotal = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Subtotal , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtSubtotal.grid(row=3,column=3)
+
+
+lblTax = Label(f1, font=( 'aria' ,16, 'bold' ),text="Tax (5%)",fg="steel blue",bd=10,anchor='w')
+lblTax.grid(row=4,column=2)
+txtTax = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Tax , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtTax.grid(row=4,column=3)
+
+lblTotal = Label(f1, font=( 'aria' ,16, 'bold' ),text="Total",fg="steel blue",bd=10,anchor='w')
+lblTotal.grid(row=5,column=2)
+txtTotal = Entry(f1,font=('ariel' ,16,'bold'), textvariable=Total , bd=6,insertwidth=4,bg="powder blue" ,justify='right')
+txtTotal.grid(row=5,column=3)
+
+lblTotal = Label(f1,text="---------------------",fg="white")
+lblTotal.grid(row=6,columnspan=3)
+
+btnTotal=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="TOTAL", bg="powder blue",command=Ref)
+btnTotal.grid(row=7, column=1)
+
+btnreset=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="RESET", bg="powder blue",command=reset)
+btnreset.grid(row=7, column=2)
+
+btnexit=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="EXIT", bg="powder blue",command=qexit)
+btnexit.grid(row=7, column=3)
+
+def price():
+    roo = Tk()
+    roo.geometry("650x200+0+0")
+    roo.title("Price List")
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="ITEM", fg="black", bd=5)
+    lblinfo.grid(row=0, column=0)
+    lblinfo = Label(roo, font=('aria', 15,'bold'), text="_____________", fg="white", anchor=W)
+    lblinfo.grid(row=0, column=2)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="PRICE", fg="black", anchor=W)
+    lblinfo.grid(row=0, column=3)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Fries", fg="steel blue", anchor=W)
+    lblinfo.grid(row=1, column=0)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Rs 25", fg="steel blue", anchor=W)
+    lblinfo.grid(row=1, column=3)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Lunch Meal", fg="steel blue", anchor=W)
+    lblinfo.grid(row=2, column=0)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Rs 40", fg="steel blue", anchor=W)
+    lblinfo.grid(row=2, column=3)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Burger", fg="steel blue", anchor=W)
+    lblinfo.grid(row=3, column=0)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Rs 35", fg="steel blue", anchor=W)
+    lblinfo.grid(row=3, column=3)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Pizza", fg="steel blue", anchor=W)
+    lblinfo.grid(row=4, column=0)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Rs 50", fg="steel blue", anchor=W)
+    lblinfo.grid(row=4, column=3)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Cheese Burger", fg="steel blue", anchor=W)
+    lblinfo.grid(row=5, column=0)
+    lblinfo = Label(roo, font=('aria', 15, 'bold'), text="Rs 30", fg="steel blue", anchor=W)
+    lblinfo.grid(row=5, column=3)
+    
+    roo.mainloop()
+
+btnprice=Button(f1,padx=16,pady=8, bd=10 ,fg="black",font=('ariel' ,16,'bold'),width=10, text="PRICE", bg="powder blue",command=price)
+btnprice.grid(row=7, column=0)
+
+root.mainloop()
---
+++ OSTL-Python-Project/README.md
@@ -0,0 +1,8 @@
+# OSTL-Python-Project
+OSTL SEM IV - Python GUI Tkinter Bill Generator Project which generates bill for Restaurant Orders
+
+Setup to run:
+
+sudo apt-get install python3-tk 
+
+python3 Billgenerator.py
